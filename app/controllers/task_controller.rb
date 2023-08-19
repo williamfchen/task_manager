b9348e2b03ba3299f50931b745423a548b2ca3de
Will Chen <wfchen1990@gmail.com>
2023-08-19 11:34:36 -0600

    Initial commit

---
+++ app/controllers/task_controller.rb
@@ -0,0 +1,8 @@
+class TasksController < ApplicationController
+  def index
+    @tasks = ['Task 1', 'Task 2', 'Task 3']
+  end
+
+  def new
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+
+  get "/", to: "welcome#index"
+
+  get "/tasks", to: "tasks#index"
+  get "/tasks/new", to: "tasks#new"
+end
