cc2ad2caf635d43072251f41891d5b66e95e4c01
Will Chen <wfchen1990@gmail.com>
2023-08-20 14:14:12 -0600

    Work through most of the project

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,27 @@
+class TasksController < ApplicationController
+  def index
+    @tasks = Task.all
+  end
+
+  def new
+  end
+
+  def create
+    task = Task.new({
+      title: params[:title],
+      description: params[:description]
+      })
+  
+    task.save
+  
+    redirect_to '/tasks'
+  end
+
+  def show
+    @task = Task.find(params[:id])
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+end
